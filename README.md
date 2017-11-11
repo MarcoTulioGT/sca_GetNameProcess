f8f322c3afc160cade633513016c4f7a511d7f43
Marco Tulio Catalan Funez <ctcatalan@tigo.com.gt>
2017-11-11 13:32:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto SOA sca_GetNameProcessCRM  de Marco Tulio Catalán"# sca_GetNameProcess" 
---
+++ scripts/dataCreateUser.sql
@@ -0,0 +1,25 @@
+     create table jenkins_user
+        (
+        id number,
+        nombre varchar2(100) ,
+        fecha date default sysdate
+        );
+
+
+         create  table jenkins_Proveedores
+        (
+        id number,
+        nombre varchar2(100) ,
+        fecha date default sysdate
+        );
+        
+
+        create  table jenkins_Tiendas
+        (
+        id number,
+        nombre varchar2(100) ,
+        fecha date default sysdate
+        );
+
+
+
